bac22a9bcc5a78f1848d4f2758a153b51f29d0ba
nt189 <133317916+nt189@users.noreply.github.com>
2025-06-09 02:54:32 -0600

    Add files via upload

---
+++ p12/product_app/backend/product-list.php
@@ -0,0 +1,8 @@
+<?php
+    use TECWEB\MYAPI\Read\Read;
+    require_once __DIR__.'/vendor/autoload.php';
+
+    $productos = new Read('marketzone');
+    $productos->list();
+    echo $productos->getData();
+?>
